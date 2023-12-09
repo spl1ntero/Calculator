026e5ec25be1a752031e2d9d7fd4d425649c0cbc
spl1ntero <sernekit77@gmail.com>
2023-12-09 18:31:31 +0300

    сделан калькулятор

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
@@ -30,14 +30,32 @@ class FeedbackForm(FlaskForm):
 class CalculatorForm(FlaskForm):
     num1 = FloatField('Число 1', validators=[InputRequired()])
     num2 = FloatField('Число 2', validators=[InputRequired()])
-    operation = SelectField('Выберите операцию', choices=[('+', '+'), ('-', '-'), ('*', '*'), ('/', '/'), ('x^2', 'x^2'),],
+    operation = SelectField('Выберите операцию', choices=[('+', '+'), ('-', '-'), ('*', '*'), ('/', '/')],
                             validators=[InputRequired()])
     submit = SubmitField('Рассчитать')
 
 
+class CalculatorForm2(FlaskForm):
+    num1 = FloatField('Число 1', validators=[InputRequired()])
+    operation2 = SelectField('Выберите операцию', choices=[('x^2', 'x^2'), ('sqrt(x)', 'sqrt(x)'), ('log(x)', 'log(x)'),
+                                                           ('exp(x)', 'exp(x)')],
+                             validators=[InputRequired()])
+    submit = SubmitField('Рассчитать')
+
 @app.route('/')
 def index():
     return render_template('index.html', title='Главная')
+
+
+@app.route('/choose', methods=['GET', 'POST'])
+def choose():
+    if request.method == 'POST':
+        choice = request.form['choice']
+        if choice == 'one':
+            return redirect(url_for('calc2'))
+        elif choice == 'two':
+            return redirect(url_for('calc'))
+    return render_template('choose.html')
 
 
 @app.route('/calc', methods=['GET', 'POST'])
@@ -54,8 +72,6 @@ def calc():
             result = num1 + num2
         elif operation == '-':
             result = num1 - num2
-        elif operation == 'x^2':
-            result = math.pow(num1, 2)
         elif operation == '*':
             result = num1 * num2
         elif operation == '/':
@@ -65,6 +81,27 @@ def calc():
                 result = "Деление на ноль невозможно"
 
     return render_template('calc.html', title='Калькулятор', form=form, result=result)
+
+
+@app.route('/calc2', methods=['GET', 'POST'])
+def calc2():
+    form = CalculatorForm2()
+    result = None
+
+    if form.validate_on_submit():
+        num1 = float(form.num1.data)
+        operation = form.operation2.data
+
+        if operation == 'x^2':
+            result = math.pow(num1, 2)
+        elif operation == 'sqrt(x)':
+            result = math.sqrt(num1)
+        elif operation == 'log(x)':
+            result = math.log(num1)
+        elif operation == 'exp(x)':
+            result = math.exp(num1)
+
+    return render_template('calc2.html', title='Калькулятор', form=form, result=result)
 
 
 @app.route('/feedback', methods=['GET', 'POST'])
